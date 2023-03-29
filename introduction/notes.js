16034523912a9cad34e68e903b20321f344cf029
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-29 21:21:03 +0600

    note

---
+++ introduction/notes.js
@@ -0,0 +1,3 @@
+/*
+JavaScript was introduced in 1995 as a way to add programs to web pages in the Netscape Navigator browser. The language has since been adopted by all other major graphical web browsers. It has made modern web applications possible—applications with which you can interact directly without doing a page reload for every action. JavaScript is also used in more traditional websites to provide various forms of interactivity and cleverness.
+*/
