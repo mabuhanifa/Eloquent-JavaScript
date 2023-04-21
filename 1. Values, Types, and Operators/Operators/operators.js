451371733bd31503e2516dc9c7c6e6b3ac19e45c
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-21 23:44:29 +0600

    Operators

---
+++ 1. Values, Types, and Operators/Operators/operators.js
@@ -0,0 +1,14 @@
+/*
+Types of JavaScript Operators
+There are different types of JavaScript operators:
+
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+Type Operators
+
+*/
